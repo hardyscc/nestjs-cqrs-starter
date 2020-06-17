4b75db7726be5835c34d3dd2e15718f0581928a9
Hardys <hardyscc@gmail.com>
2020-06-17 21:52:39 +0800

    chore: suppress eslint warning

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,5 +20,6 @@ module.exports = {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
   },
 };
